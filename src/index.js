704d2a4b5e0c47d48d5d13fb7650aeed07850704
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-20 12:27:12 -0400

    Basic html form submission testing dynamic creation of compositional project objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+import { Project } from "./modules/Project";
+
+let projectList = [];
+
+const projects = document.getElementById("projects-list");
+const formInput = document.getElementById("form-input");
+const submitBtn = document.getElementById("submitBtn");
+
+function handleSubmit() {
+  let input = formInput.value;
+
+  projectList.push(Project(input));
+}
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  handleSubmit();
+  console.log(projectList);
+  projects.innerText = "";
+  renderProjects();
+});
+
 console.log("Build successful");
+
+let newProject = Project("Test Project");
+projectList.push(newProject);
+
+console.log(projectList);
+
+function renderProjects() {
+  for (let i = 0; i < projectList.length; i++) {
+    let line = document.createElement("li");
+    line.innerText = `${projectList[i].name}`;
+    projects.appendChild(line);
+  }
+}
+
+renderProjects();
---
+++ src/modules/Project.js
@@ -0,0 +1,26 @@
+const Project = (name) => {
+  let project = {
+    name,
+    tasks: [],
+  };
+
+  return Object.assign({ ...project }, newTaskCreator(), taskRemover());
+};
+
+const newTaskCreator = (project) => ({
+  addTask: (title, description, dueDate, priority) =>
+    project.tasks.push({
+      title,
+      description,
+      dueDate,
+      priority,
+    }),
+});
+
+const taskRemover = (project) => ({
+  deleteTask: (index) => {
+    project.tasks.splice(index, 1);
+  },
+});
+
+export { Project };
